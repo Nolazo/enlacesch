d13686cf215f0b7db82d5e90da4abf1e01c966bc
unknown <j.manuelvaldebenito@gmail.com>
2025-04-08 10:37:29 -0400

    FEAT: Se agregaron estilos al navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,55 +1,84 @@
 import React from 'react'
+import {useState} from 'react'
 import 'remixicon/fonts/remixicon.css'
-// import './navbar.css'; // Assuming you have some CSS for styling
 
 const Navbar = () => {
+  const [active, setActive] = useState(true)
+
   return (
-    <nav class="container flex items-center justify-between h-16">
-      <div class="bg-red-400 absolute top-0 left-0
-      min-h-[80vh] w-full duration-500 ease-out overflow-hidden z-20">
-        
-        <a href="/" class="navbar-logo">
-          Logo
-        </a>
-        <ul class="flex flex-col items-center gap-8 pt-20">
-          <li>
-            <a href="/home" class="nav-link active">
+    <nav className="container flex items-center justify-between h-16 sm:h-20">
+      {/* HAMBURGER */}
+      <div className="text-xl cursor-pointer sm:text-2xl lg:hidden">
+        <i onClick={()=> {
+          setActive(!active)
+        }} className="ri-menu-4-line"></i>
+      </div>
+      {/* LOGO */}
+      <img src="" alt="" className="w-28 md:w-40"/>
+      
+      <div className={`${active === true ? "left-[-100%]" : "left-[0]"} bg-red-400 absolute top-0 min-h-[80vh]
+         w-full duration-500 ease-out overflow-hidden z-20 md:px-10 
+         md:w-1/2 lg:static lg:min-h-fit lg:bg-transparent lg:w-auto`}>
+        {/* MENU ITEMS */}
+        <ul className="flex flex-col items-center gap-8 pt-20 lg:pt-0 lg:flex-row">
+          <li onClick={()=> {setActive(!active)}}>
+            <a href="/home" className="nav-link active">
               Home
             </a>
           </li>
-          <li>
-            <a href="/about" class="nav-link">
+          <li onClick={()=> {setActive(!active)}}>
+            <a href="/about" className="nav-link">
               About
             </a>
           </li>
-          <li>
-            <a href="/services" class="nav-link">
+          <li onClick={()=> {setActive(!active)}}>
+            <a href="/services" className="nav-link">
               Services
             </a>
           </li>
-          <li>
-            <a href="/contact" class="nav-link">
+          <li onClick={()=> {setActive(!active)}}>
+            <a href="/contact" className="nav-link">
               Contact
             </a>
           </li>
         </ul>
 
-        <div class="text-xl cursor-pointer">
-          <i class="ri-arrow-right-down-line"></i>
+        <div className="text-xl cursor-pointer absolute top-5 right-5 sm:text-2xl">
+          <i onClick={()=> {
+          setActive(!active)}} 
+          className={`ri-close-large-line`}></i>
         </div>
 
-        <div>
-          <i></i>
-          <i></i>
-          <div>
-            <i></i>
+        <div className="flex items-center gap-5 text-xl cursor-pointer pt-20 sm:text-2xl">
+          <i className="ri-search-line"></i>
+          <i className="ri-user-line"></i>
+          <div className="relative">
+            <i className="ri-heart-line"></i>
+            <span className="font-bold absolute top-0 left-4 bg-red-500
+            text-yellow-50 text-xs w-5 h-5 rounded-full grid place-items-center">0</span>
           </div>
-          <div>
-            <i></i>
+          <div className="relative">
+            <i className="ri-shopping-cart-line"></i>
+            <span className="font-bold absolute top-0 left-4 bg-red-500
+            text-yellow-50 text-xs w-5 h-5 rounded-full grid place-items-center">0</span>
           </div>
         </div>
-
       </div>
+
+      <div className="flex items-center gap-5 text-xl cursor-pointer sm:text-2xl">
+          <i className="hidden ri-search-line"></i>
+          <i className="hidden ri-user-line"></i>
+          <div className="hidden relative">
+            <i className="ri-heart-line"></i>
+            <span className="font-bold absolute top-0 left-4 bg-red-500
+            text-yellow-50 text-xs w-5 h-5 rounded-full grid place-items-center">0</span>
+          </div>
+          <div className="relative">
+            <i className="ri-shopping-cart-line"></i>
+            <span className="font-bold absolute top-0 left-4 bg-red-500
+            text-yellow-50 text-xs w-5 h-5 rounded-full grid place-items-center">0</span>
+          </div>
+        </div>
     </nav>
   )
 }
